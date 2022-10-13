d52a5970aa6329f51bb3ab138335a1d200ee340d
benb0201 <111536131+benb0201@users.noreply.github.com>
2022-10-13 01:11:24 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require("http")
-
-const server = http.createServer((req,res) =>{
-    res.writeHead(200)
-    res.end("Hello World")
-})
-server.listen(8080);
-console.log("[app] Server started.")
