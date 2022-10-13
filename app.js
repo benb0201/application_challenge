8cda26971f5b16bd7ad94fda9f675d125d947ef1
benb0201 <111536131+benb0201@users.noreply.github.com>
2022-10-13 01:13:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -22,23 +22,3 @@ fs.readFile(__dirname + "/index.html")
         console.error(`Could not read index.html file: ${err}`);
         process.exit(1);
     });
-
-// const server = http.createServer((req,res) =>{
-//     fs.readFile(__dirname + "/index.html")
-//     .then(contents => {
-//         res.setHeader("Content-Type", "text/html");
-//         res.writeHead(200);
-//         res.end(contents);
-//     })
-// })
-// server.listen(8080);
-// console.log("[app] Server started.")
-
-// const requestListener = function (req, res) {
-//     fs.readFile(__dirname + "/index.html")
-//         .then(contents => {
-//             res.setHeader("Content-Type", "text/html");
-//             res.writeHead(200);
-//             res.end(contents);
-//         })
-// };
